fc4002df67de1e844f5e9450afb9bb79bf64ce0b
manoj-406 <manojgonchal111@gmail.com>
2024-09-01 11:39:09 +0530

    added new solved problems in sep1

--- test.py
+++ test.py
@@ -1,11 +1,26 @@
-""" docstring"""
-n = int(input())
-for i in range (n):
-    a , b = map(int,input().split())
-    if b == 0:
-        print(0)
-    else:
-        c = a // b
-        f = c * b
-        print (f)
-# End-of-file (EOF)
+'''This is a program to find weather the number is prime or not
+
+def is_prime(number):
+    for index in range(2, number):
+        if number % index ==0:
+            return False
+
+    return True
+
+def factorial(num):
+    index = 0
+    for i  in range(num+1):
+        index += i * i 
+        return index
+
+num = 5
+print(factorial(num))
+'''
+# cook your dish here
+t = int(input())
+for i in range(t):
+    a, b = map(int,input().split())
+    x= 6 / a
+    print(b * x)
+  
+
